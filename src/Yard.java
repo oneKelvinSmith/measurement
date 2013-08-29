ce335dc8c00f7f9cf0875d8b8ffceefa90488be9
Kelvin Smith <ksmith@thoughtworks.com>
2013-08-29 11:06:29 +0200

    Pat's madness.

--- src/Yard.java
+++ src/Yard.java
@@ -1,13 +1,10 @@
 // understands length in yards
-public class Yard extends Length {
-
-    public static final int INCHES_IN_A_YARD = 36;
-
+public class Yard {
     public Yard(float value) {
-        super(value);
     }
 
-    protected float toInch() {
-        return value * INCHES_IN_A_YARD;
+    @Override
+    public boolean equals(Object o) {
+        return !o.getClass().equals(new Yard(1).getClass());
     }
 }
